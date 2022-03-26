8f29d5c74202f7102de6579becbc826b0c9e5b6b
Lisieux Almeida <lisieux.almeida@gmail.com>
2022-03-26 22:16:23 +0000

    first commit

---
+++ src/Components/Extrato/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Box, Botao } from '../../Components/UI/icones';
+import {extratoLista} from '../../info'
+import Items from '../Items/items';
+
+
+const Extrato = () => {
+
+    return(
+        <Box>
+            {
+                extratoLista.updates.map(({id, type, from, value, date}) => {
+                    return(
+                        <Items key={id} type={type} from={from} value={value} date={date} />
+                    );
+                })
+            }
+            <Botao>Ver Mais</Botao>
+        </Box>
+
+    );
+}
+
+export default Extrato;
